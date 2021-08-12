3593ce2559cecc4269cee115d930e35f06a55020
Turonk <gorlov.a@mail.ru>
2021-08-12 21:10:39 +0300

    add show func

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import datetime
+import datetime as dt
 
 FORMAT = "%H:%M:%S"
-WEIGHT = 75
-HEIGHT = 175
-K_1 = 0.035
-K_2 = 0.029
+WEIGHT = 75  # Вес
+HEIGHT = 175  # Рост
+K_1 = 0.035  # Коэффициент для подсчета калорий
+K_2 = 0.029  # Коэффициент для подсчета калорий
+STEP_M = 0.65  # Длина шага в метрах
 
 storage_dict = {}
 
@@ -29,7 +30,7 @@ def check_correct_time(time):
 
 
 def get_step_day(steps):
-    """Получить количество пройденных шагов за день"""
+    """Получить количество пройденных шагов за день."""
     day_steps = 0
     for key, value in storage_dict.items():
         day_steps += value['steps']
@@ -38,7 +39,7 @@ def get_step_day(steps):
 
 def get_distance(steps):
     """Получить дистанцию пройденного пути в км."""
-    return (steps * 0.65)/1000
+    return (steps * STEP_M)/1000
 
 
 def get_spent_calories(dist, current_time):
@@ -48,25 +49,47 @@ def get_spent_calories(dist, current_time):
     return (K_1*WEIGHT+(mean_speed**2//HEIGHT)*K_2*WEIGHT)*time*60
 
 
+def get_achievement(dist):
+    """Получение поздравления за пройденную дистанцию."""
+    if dist < 2:
+        return 'Лежать тоже полезно. Главное — участие, а не победа!'
+
+    if dist < 3.9:
+        return 'Маловато, но завтра наверстаем!'
+
+    if dist < 6.5:
+        return 'Неплохо! День был продуктивным.'
+
+    return 'Отличный результат! Цель достигнута.'
+
+
+def show_on_display(time, steps, dist, calories, achiev):
+    """Вывести на экран результаты вычислений"""
+    print(f'''
+Время: {time}.
+За сегодня вы прошли {steps} шагов.
+Дистанция составила {dist:.2f} км.
+Вы сожгли {calories:.2f} ккал.
+{achiev}.
+        ''')
+
+
 def accept_package(data):
     """Обработать пакет данных."""
     time, steps = data
-    pack_time = datetime.datetime.strptime(time, FORMAT).time()
+    pack_time = dt.datetime.strptime(time, FORMAT).time()
     if not(check_correct_data(data) and check_correct_time(pack_time)):
         return 'Некорректный пакет'
 
     day_steps = get_step_day(steps)
     dist = get_distance(day_steps)
     spent_calories = get_spent_calories(dist, pack_time)
+    achievement = get_achievement(dist)
+    show_on_display(pack_time, day_steps, dist,
+                    spent_calories, achievement)
 
     storage_dict[pack_time] = {'steps': steps}
 
-    print(f'''
-        Время: {pack_time}.
-        За сегодня вы прошли {day_steps} шагов.
-        Дистанция составила {dist:.2f} км.
-        Вы сожгли {spent_calories:.2f} ккал.
-        ''')
     return storage_dict
 
 
